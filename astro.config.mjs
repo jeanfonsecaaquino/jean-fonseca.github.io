3dcecea7c402425befc22c8e73743d3dd831f40d
Jean Aquino <jean.fonseca.aquino@gmail.com>
2026-03-12 20:38:03 -0300

    fix: update site and base path for github project pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jean-fonseca.github.io',
+  site: 'https://jeanfonsecaaquino.github.io',
+  base: '/jean-fonseca.github.io',
 });
